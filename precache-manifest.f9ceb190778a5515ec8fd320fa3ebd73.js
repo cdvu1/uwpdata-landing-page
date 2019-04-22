d78b9b7c6f8685dc24a468ebc48178a9f275a066
Cecilia Vu <cdvu@uw.edu>
2019-04-22 16:00:18 -0700

    ‘committing

---
+++ precache-manifest.f9ceb190778a5515ec8fd320fa3ebd73.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f413dd7630b2be036e7b57d69400a09a",
+    "url": "/uwpdata-landing-page/index.html"
+  },
+  {
+    "revision": "54c6df7ac62e1e838ce1",
+    "url": "/uwpdata-landing-page/static/css/main.af7131e3.chunk.css"
+  },
+  {
+    "revision": "689dbb00316a3f35c446",
+    "url": "/uwpdata-landing-page/static/js/2.6b4dd69a.chunk.js"
+  },
+  {
+    "revision": "54c6df7ac62e1e838ce1",
+    "url": "/uwpdata-landing-page/static/js/main.260dbc36.chunk.js"
+  },
+  {
+    "revision": "e44abae509ed6e3f4df1",
+    "url": "/uwpdata-landing-page/static/js/runtime~main.7850949a.js"
+  },
+  {
+    "revision": "dbc541e1c70e53ee31b212eafd3a8890",
+    "url": "/uwpdata-landing-page/static/media/New_patch.dbc541e1.png"
+  },
+  {
+    "revision": "af78362ac60911535c9f39134bcb4fb8",
+    "url": "/uwpdata-landing-page/static/media/access-control.af78362a.png"
+  },
+  {
+    "revision": "8da082b6f208d935ed56fd428f2aab2d",
+    "url": "/uwpdata-landing-page/static/media/admin.8da082b6.jpg"
+  },
+  {
+    "revision": "8ff1459a2a4e1d6079efd7b8755d1394",
+    "url": "/uwpdata-landing-page/static/media/ben.8ff1459a.jpeg"
+  },
+  {
+    "revision": "a87a2e717e2e1a05f341b23e02acb4bb",
+    "url": "/uwpdata-landing-page/static/media/cece.a87a2e71.jpg"
+  },
+  {
+    "revision": "66d5880f14084a81ed1d2f50698c9442",
+    "url": "/uwpdata-landing-page/static/media/godwin.66d5880f.jpeg"
+  },
+  {
+    "revision": "ec979e6439ebd8c9c487c7a3938ba3ee",
+    "url": "/uwpdata-landing-page/static/media/maggie.ec979e64.jpeg"
+  },
+  {
+    "revision": "83d6ec937b8030a768e3dbb92be9b0dc",
+    "url": "/uwpdata-landing-page/static/media/research.83d6ec93.png"
+  }
+]);
